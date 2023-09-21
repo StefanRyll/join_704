c131b400892808b27a99136fcf1a7f200262ad0d
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2023-09-21 18:37:11 +0200

    createTask wird erstellt

--- RomansKram/script.js
+++ RomansKram/script.js
@@ -1,15 +1,27 @@
 class Task{
-    constructor(creator = 'MrKnister', title, date =, desc, prio) {
-        this.creator = creator;
+    constructor(creator, title,  desc, date = "No need", prio, Cathe ) {
+        this.creator =creator;
         this.title = title;
         this.worker = [];
-        this.date = date;
         this.desc = desc;
+        this.date = date;
         this.prio = prio;
         this.Categroy = Categroy;
         this.subTasks = [];
     }
 }
+let tasklist = [];
+function createTask(){
+    const title = document.getElementById("addTaskTitle").value;
+    const desc = document.getElementById("addTaskDescription").value;
+    const date = document.getElementById("addTaskDescription").value;
+    const prio = "Aber dalli!"; // Ersetzen wenn du weißt wie!
+    let newTask = new Task(title, desc, date, prio );
+    tasklist.push(newTask)
+    console.log(tasklist);
+}
+
+
 
 function logEnding(x){
     console.log(x);
