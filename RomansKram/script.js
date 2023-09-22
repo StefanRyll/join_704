e33655ab11f917380c80fa83607ebdebca6db908
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2023-09-22 13:32:02 +0200

    ContactFormular
    Funktioniert sieht aber so "Weiß nich digga" aus

--- RomansKram/script.js
+++ RomansKram/script.js
@@ -1,38 +1,36 @@
-class Task{
-    constructor(creator, title, worker, desc, date = "No need", prio, Categroy ) {
-        this.creator = creator;
-        this.title = title;
-        this.worker = worker;
-        this.desc = desc;
-        this.date = date;
-        this.prio = prio;
-        this.Categroy = Categroy;
-        this.subTasks = [];
+class Contact{
+    constructor(name, email, phone){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+
     }
-}
-let tasklist = [];
-function createTask(){
-    const creator = "Ich";
-    const title = document.getElementById("addTaskTitle").value;
-    const worker = document.getElementById('assignTaskToContacts').value;
-    const desc = document.getElementById("addTaskDescription").value;
-    const date = document.getElementById("addTaskDescription").value;
-    const prio = getPrio();
-    const category = document.getElementById('addTaskCategory').value;
-    let newTask = new Task(creator, title, worker, desc, date, prio, category);
-    tasklist.push(newTask)
-    console.log(newTask);
+    asCard(){
+        return /*html*/`
+            <div>
+                <img src="./IMG/UserIMG.png" alt="">
+                <div>
+                    <p>${this.name}</p>
+                    <p>${this.email}</p>
+                </div>
+            </div>
+        `
+    }
+
 }
 
-function getPrio(){
-    let prio = document.getElementsByName('prio');
-    for (let i = 0; i < prio.length; i++) {
-        if (prio[i].checked){
-            return prio[i].value;
-        }
-    }
-}
+let contactList = [];
+let Jan = new Contact("Jan", "jan@da.com", "012335")
 
+function createContact(){
+    const name = document.getElementById('inputName').value;
+    const email = document.getElementById('inputEmail').value;
+    const phone = document.getElementById('inputPhone').value;
+    let newContact = new Contact(name, email, phone)
+    contactList.push(newContact);
+    // console.log(newContact);
+}
+console.log(Jan.asCard());
 function logEnding(x){
     console.log(x);
 }
