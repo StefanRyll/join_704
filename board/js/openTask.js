ec8e992468889c2ef51582c139d8c640e8d28e57
Dominik Hucka <dominikhucka@googlemail.com>
2023-09-28 21:29:44 +0200

    add task design 90% completed / next few days edit function

--- board/js/openTask.js
+++ board/js/openTask.js
@@ -15,11 +15,13 @@ function generateHTMLaddTask() {
         <div id="slideAddTask" class="bg-task">
           <div class="add-task">
            ${generateHTMLLeftSide()}
-            <svg xmlns="http://www.w3.org/2000/svg" width="2" height="426" viewBox="0 0 2 426" fill="none">
-              <path d="M1.24805 1L1.24854 425" stroke="#D1D1D1" stroke-linecap="round"/>
-            </svg>
-            <!-- <div class="seporator-add-task"></div> -->
+           <div class="seperator-add-task">
+              <svg xmlns="http://www.w3.org/2000/svg" width="2" height="426" viewBox="0 0 2 426" fill="none">
+                <path d="M1.24805 1L1.24854 425" stroke="#D1D1D1" stroke-linecap="round"/>
+              </svg>
+            </div>
             ${generateHTMLRightSide()}
+            ${generateHTMLCloseButtonInSVG()}
           </div>
         </div>
     `
@@ -28,20 +30,13 @@ function generateHTMLaddTask() {
 
 function generateHTMLLeftSide() {
   return /*html*/`
-        <div class="left-side">
+        <div class="headline-add-task">
           <h3>Add Task</h3>
-          <form class="input-title board-task-input">
-              <label for="pflichtfeld">Title<sup>*</sup></label>
-              <input type="text" id="" name="" required  placeholder="Enter a title">
-          </form>
-          <div class="input-description">
-              <p>Description</p>
-              <textarea name="" id="" cols="30" rows="10" placeholder="Enter a Description"></textarea>
-          </div>
-          <div class="assigned-contact board-task-input">
-              <p>Assigned to</p>
-              <input type="button" value="Select contacts to assign" id="">
-          </div>
+        </div>
+        <div class="left-side">
+          ${generateHTMLTitle()}
+          ${generateHTMLDescription()}
+          ${generateHTMLAssignedTo()}
           <div class="field-required-text">
             <sub>*</sub><span>This field is required</span>
           </div>
@@ -50,30 +45,80 @@ function generateHTMLLeftSide() {
 }
 
 
+function generateHTMLTitle() {
+  return /*html*/`
+          <form class="input-title board-task-input">
+              <label for="pflichtfeld">Title<sup>*</sup></label>
+              <input type="text" id="" name="" required  placeholder="Enter a title">
+          </form>
+  `
+}
+
+
+function generateHTMLDescription() {
+  return /*html*/`
+          <div class="input-description">
+              <p>Description</p>
+              <textarea name="" id="" cols="30" rows="10" placeholder="Enter a Description"></textarea>
+          </div>
+  `
+}
+
+
+function generateHTMLAssignedTo() {
+  return /*html*/`
+          <div class="assigned-contact board-task-input">
+              <p>Assigned to</p>
+              <div id="selectContacts" class="assign-container">
+                <input  onclick="openContactsAssign()" type="button" value="Select contacts to assign" id="">
+                 <img onclick="openContactsAssign()" src="/assets/img/arrow_drop_downaa.png" alt="">
+                <!-- <svg class="assign-down" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      <mask id="mask0_88947_4985" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                        <rect width="24" height="24" fill="#D9D9D9"/>
+                      </mask>
+                      <g mask="url(#mask0_88947_4985)">
+                          <path d="M11.3 14.3L8.69998 11.7C8.38331 11.3833 8.31248 11.0208 8.48748 10.6125C8.66248 10.2042 8.97498 10 9.42498 10H14.575C15.025 10 15.3375 10.2042 15.5125 10.6125C15.6875 11.0208 15.6166 11.3833 15.3 11.7L12.7 14.3C12.6 14.4 12.4916 14.475 12.375 14.525C12.2583 14.575 12.1333 14.6 12 14.6C11.8666 14.6 11.7416 14.575 11.625 14.525C11.5083 14.475 11.4 14.4 11.3 14.3Z" fill="#2A3647"/>
+                      </g>
+                </svg>
+                <svg class="assign-up" xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                      <mask id="mask0_75597_14130" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
+                        <rect y="0.96582" width="24" height="24" fill="#D9D9D9"/>
+                      </mask>
+                      <g mask="url(#mask0_75597_14130)">
+                        <path class="d-none" d="M12.6995 11.2659L15.2995 13.8659C15.6162 14.1826 15.687 14.5451 15.512 14.9534C15.337 15.3618 15.0245 15.5659 14.5745 15.5659H9.42454C8.97454 15.5659 8.66204 15.3618 8.48704 14.9534C8.31204 14.5451 8.38287 14.1826 8.69954 13.8659L11.2995 11.2659C11.3995 11.1659 11.5079 11.0909 11.6245 11.0409C11.7412 10.9909 11.8662 10.9659 11.9995 10.9659C12.1329 10.9659 12.2579 10.9909 12.3745 11.0409C12.4912 11.0909 12.5995 11.1659 12.6995 11.2659Z" fill="#2A3647"/>
+                      </g>
+                </svg> -->
+              </div>
+              <div id="closeContacts" class="assign-container d-none">
+                  <input type="text">
+                  <img src="/assets/img/arrow_dropdown.png" alt="">
+                  <div class="checkbox-container">
+                    <label>
+                      <span>Roman</span>
+                      <input type="checkbox" name="" id="">
+                    </label>
+                  </div>
+                </div>
+          </div>
+  ` 
+}
+
+
+function openContactsAssign() {
+  document.getElementById('selectContacts').classList.add('d-none');
+  document.getElementById('closeContacts').classList.remove('d-none');
+}
+
+
 function generateHTMLRightSide() {
   return /*html*/`
       <div class="right-side">
-            ${generateHTMLCloseButtonInSVG()}
             ${generateHTMLDateForm()}
             ${generateHTMLPrioCategory()}
             ${generateHTMLCategory()}
             ${generateHTMLSubtask()}
             ${generateHTMLButtons()}
       </div>
-    `
-}
-
-
-function generateHTMLCloseButtonInSVG() {
-  return /*html*/`
-            <svg onclick="closeAddTask()" class="close-button-add-task cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <mask id="mask0_87491_5574" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                  <rect width="24" height="24" fill="#D9D9D9"/>
-                </mask>
-                <g mask="url(#mask0_87491_5574)">
-                  <path d="M12 13.4L7.1 18.3C6.91667 18.4834 6.68333 18.575 6.4 18.575C6.11667 18.575 5.88333 18.4834 5.7 18.3C5.51667 18.1167 5.425 17.8834 5.425 17.6C5.425 17.3167 5.51667 17.0834 5.7 16.9L10.6 12L5.7 7.10005C5.51667 6.91672 5.425 6.68338 5.425 6.40005C5.425 6.11672 5.51667 5.88338 5.7 5.70005C5.88333 5.51672 6.11667 5.42505 6.4 5.42505C6.68333 5.42505 6.91667 5.51672 7.1 5.70005L12 10.6L16.9 5.70005C17.0833 5.51672 17.3167 5.42505 17.6 5.42505C17.8833 5.42505 18.1167 5.51672 18.3 5.70005C18.4833 5.88338 18.575 6.11672 18.575 6.40005C18.575 6.68338 18.4833 6.91672 18.3 7.10005L13.4 12L18.3 16.9C18.4833 17.0834 18.575 17.3167 18.575 17.6C18.575 17.8834 18.4833 18.1167 18.3 18.3C18.1167 18.4834 17.8833 18.575 17.6 18.575C17.3167 18.575 17.0833 18.4834 16.9 18.3L12 13.4Z" fill="#2A3647"/>
-                </g>
-            </svg>
     `
 }
 
@@ -160,8 +205,10 @@ function generateHTMLPrio() {
 
 function generateHTMLCategory() {
   return /*html*/`
-    <div class="category board-task-input">
-      <label for="category">Category<sub>*</sub></label>
+    <div class="category board-task-input-button-style button-hover">
+      <div class="category-sub">
+        <label for="category">Category<sub>*</sub></label>
+      </div>
       <input type="button" value="Select task category">
     </div>
   `
@@ -170,7 +217,7 @@ function generateHTMLCategory() {
 
 function generateHTMLSubtask() {
   return /*html*/`
-    <div class=" subtasks board-task-input">
+    <div class=" subtasks board-task-input-button-style button-hover">
       <p>Subtasks</p>
       <input type="button" value="Add new subtask">
     </div>
@@ -181,9 +228,12 @@ function generateHTMLSubtask() {
 function generateHTMLButtons() {
   return /*html*/`
       <div class="bottom-button">
-        <button class="cancel-button">
+        <button onclick="closeAddTask()" class="cancel-button">
           <span>Cancel</span>
-          <img src="/assets/img/cancel.png" alt="cancel Button in add task">
+            <svg class="cancel-button-stroke" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+            <line x1="0" y1="0" x2="24" y2="24" stroke="black" stroke-width="2" class="change-color" />
+            <line x1="24" y1="0" x2="0" y2="24" stroke="black" stroke-width="2"  class="change-color" />
+          </svg>
         </button>
         <button class="create-task-button">
           <span>Create Task</span>
@@ -194,6 +244,21 @@ function generateHTMLButtons() {
 }
 
 
+function generateHTMLCloseButtonInSVG() {
+  return /*html*/`
+          <div class="style-closebutton">
+            <svg onclick="closeAddTask()" class="close-button-add-task cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <mask id="mask0_87491_5574" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                  <rect width="24" height="24" fill="#D9D9D9"/>
+                </mask>
+                <g mask="url(#mask0_87491_5574)">
+                  <path d="M12 13.4L7.1 18.3C6.91667 18.4834 6.68333 18.575 6.4 18.575C6.11667 18.575 5.88333 18.4834 5.7 18.3C5.51667 18.1167 5.425 17.8834 5.425 17.6C5.425 17.3167 5.51667 17.0834 5.7 16.9L10.6 12L5.7 7.10005C5.51667 6.91672 5.425 6.68338 5.425 6.40005C5.425 6.11672 5.51667 5.88338 5.7 5.70005C5.88333 5.51672 6.11667 5.42505 6.4 5.42505C6.68333 5.42505 6.91667 5.51672 7.1 5.70005L12 10.6L16.9 5.70005C17.0833 5.51672 17.3167 5.42505 17.6 5.42505C17.8833 5.42505 18.1167 5.51672 18.3 5.70005C18.4833 5.88338 18.575 6.11672 18.575 6.40005C18.575 6.68338 18.4833 6.91672 18.3 7.10005L13.4 12L18.3 16.9C18.4833 17.0834 18.575 17.3167 18.575 17.6C18.575 17.8834 18.4833 18.1167 18.3 18.3C18.1167 18.4834 17.8833 18.575 17.6 18.575C17.3167 18.575 17.0833 18.4834 16.9 18.3L12 13.4Z" fill="#2A3647"/>
+                </g>
+            </svg>
+          </div>
+    `
+}
+
 function openAddTask() {
   slideAddTask = document.getElementById('slideAddTask').classList.add('show-bg-task');
 }
@@ -202,5 +267,7 @@ function openAddTask() {
 function closeAddTask() {
   slideAddTask = document.getElementById('slideAddTask').classList.remove('show-bg-task');
 }
+
+
 
 
