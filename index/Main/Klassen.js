86fb31a4070d03ab8af3031442b91ba1706b6f38
StefanRyll <142165351+StefanRyll@users.noreply.github.com>
2023-10-03 11:51:19 +0200

    update signup

--- index/Main/Klassen.js
+++ index/Main/Klassen.js
@@ -36,10 +36,10 @@ class Page {
                         <img src="./IMG/vector-5.png">
                     </div>
                     <form class="login-container" onsubmit="logInUser()">
-                        <div class="frame-14"><input type="email" class="frame-157" placeholder="Email" id="loginEmail">
+                        <div class="frame-14"><input required type="email" class="frame-157" placeholder="Email" id="loginEmail">
                             <img class="input-icon" src="./IMG/mail.png"> 
                         </div>                       
-                        <div class="frame-14" onclick="visibility()"><input type="password" class="frame-158" placeholder="Password" id="loginPassword">
+                        <div class="frame-14" onclick="visibility()"><input required type="password" class="frame-158" placeholder="Password" id="loginPassword">
                             <img class="input-icon curser-pointer" id="pass-status" src="./IMG/lock.png" onclick="viewPassword()">                      
                             <img class="curser-pointer input-icon d-none" id="pass-status-eye" src="./IMG/visibility_off.png" onclick="viewPassword()">                      
                         </div>
@@ -135,23 +135,60 @@ class Page {
         return /*html*/ `
             <div id="signUpWindow" class="signUpWindow">
   
-                    <button class="backBtn" onclick="startPage2()">
+                    <a class="backBtn" onclick="startPage2()">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
                             <path d="M10.4373 14.6667H25.3333C26.0696 14.6667 26.6666 15.2637 26.6666 16.0001C26.6666 16.7364 26.0696 17.3334 25.3333 17.3334H10.4373L16.6466 23.5427C17.1672 24.0634 17.1672 24.9074 16.6466 25.4281C16.126 25.9487 15.2819 25.9487 14.7613 25.4281L6.74746 17.4143C5.96642 16.6332 5.96642 15.3669 6.74747 14.5858L14.7613 6.57206C15.2819 6.05144 16.126 6.05144 16.6466 6.57206C17.1672 7.09268 17.1672 7.93677 16.6466 8.45739L10.4373 14.6667Z" fill="#29ABE2"/>
                         </svg>
-                    </button>
+                    </a>
                     <div class="frame-159">
                         <h1>Sign Up</h1>
                         <img src="./IMG/vector-5.png">
                     </div>
-                <form class="accountForm" onsubmit="createAccount1()">
+                <!-- <form class="accountForm" onsubmit="createAccount1()">
                     <div class="signUpInputArea"><input required type="text" id="signUpInputName" placeholder="Name" class="signUpInput"></div>
                     <div class="signUpInputArea"><input required type="email" id="signUpInputEmail" placeholder="Email" class="signUpInput"></div>
                     <div class="signUpInputArea"><input required type="password" id="signUpInputPassword" placeholder="Password" class="signUpInput"></div>
                     <div id="passwordCheckArea" class="signUpInputArea"><input required type="password" id="signUpInputPassword2" placeholder="Confirm Password" class="signUpInput"></div>
                     <label><input required type="checkbox" name="ppCheck" id="ppCheck">I accept the <a href="">Privacy policy</a></label>
                     <button type="submit">Sign Up</button>
-                </form>
+                </form> -->
+                <form class="login-container" onsubmit="createAccount1()">
+                        <div class="frame-14"><input required type="text" class="frame-157" placeholder="Name" id="signUpInputName">
+                            <img class="input-icon" src="./IMG/person.png"> 
+                        </div>
+                        <div class="frame-14"><input required type="email" class="frame-157" placeholder="Email" id="signUpInputEmail">
+                            <img class="input-icon" src="./IMG/mail.png"> 
+                        </div>
+                        <div class="frame-14" onclick="visibility()"><input type="password" class="frame-158" placeholder="Password" id="signUpInputPassword">
+                            <img class="input-icon curser-pointer" id="pass-status" src="./IMG/lock.png" onclick="viewPassword()">                      
+                            <img class="curser-pointer input-icon d-none" id="pass-status-eye" src="./IMG/visibility_off.png" onclick="viewPassword()">                      
+                        </div>
+                        <div class="frame-14" onclick="visibility()"><input type="password" class="frame-158" placeholder="Confirm Password" id="signUpInputPassword2">
+                            <img class="input-icon curser-pointer" id="pass-status" src="./IMG/lock.png" onclick="viewPassword()">                      
+                            <img class="curser-pointer input-icon d-none" id="pass-status-eye" src="./IMG/visibility_off.png" onclick="viewPassword()">                      
+                        </div>
+                        <label class="sign-up-privacy-policy">
+                        <div class="check-box" id="checkbox" onclick="checkboxActivate()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                                <rect x="4" y="4.5" width="16" height="16" rx="3" stroke="#2A3647" stroke-width="2"/>
+                            </svg>
+                        </div>
+                        <div class="check-box d-none" id="checkbox-active" onclick="checkboxDeactivate()">
+                            <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="Group 19">
+                                    <path id="Vector 9" d="M17 8.96582V14.9658C17 16.6227 15.6569 17.9658 14 17.9658H4C2.34315 17.9658 1 16.6227 1 14.9658V4.96582C1 3.30897 2.34315 1.96582 4 1.96582H12" stroke="#2A3647" stroke-width="2" stroke-linecap="round"/>
+                                    <path id="Vector 17" d="M5 9.96582L9 13.9658L17 2.46582" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                            </svg>
+                        </div>
+                            I accept the&nbsp;<a class="link-style" onclick="privacyPage()"> Privacy policy</a>
+                        </label>
+                        <div class="frame-176">
+                            <div class="loginButtons">
+                            <button class="login-btn" type="submit">Sign Up</button>
+                            </div>
+                        </div>
+                    </form>
             </div>
         
 
