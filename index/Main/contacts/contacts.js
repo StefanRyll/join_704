c808beb67741b27e5dc5db503144c8e2015a4b6d
StefanRyll <142165351+StefanRyll@users.noreply.github.com>
2023-10-05 12:20:06 +0200

    updater script contacts

---
+++ index/Main/contacts/contacts.js
@@ -0,0 +1,35 @@
+let contacts = [{
+    "name": "Anton Mayer",
+    "email": "antom@gmail.com",
+}, {
+    "name": "Anja Schulz",
+    "email": "schulz@hotmail.com",
+}, {
+    "name": "Benedikt Ziegler",
+    "email": "benedikt@gmail.com",
+}, {
+    "name": "David Eisenberg",
+    "email": "davidberg@gmail.com",
+}, {
+    "name": "Eva Fischer",
+    "email": "eva@gmail.com",
+}, {
+    "name": "Emmanuel Mauer",
+    "email": "emmanuelma@gmail.com",
+}, {
+    "name": "Marcel Bauer",
+    "email": "bauer@gmail.com",
+}, {
+    "name": "Tatiana Wolf",
+    "email": "wolf@gmail.com",
+}, ]
+
+function initContacts() {
+    let contactsList = document.getElementById('contactsList');
+    contactsList.innerHTML = '';
+
+    for (let i = 0; i < contacts.length; i++) {
+        const contacts = contacts[i];
+
+    }
+}
