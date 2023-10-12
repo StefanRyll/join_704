da38ac1718dc306f8341394a17c854b1cf9dd883
StefanRyll <142165351+StefanRyll@users.noreply.github.com>
2023-10-12 15:49:03 +0200

    update contacts

--- index/Main/contacts/contacts.js
+++ index/Main/contacts/contacts.js
@@ -1,38 +1,38 @@
 let user = [{
-    name: "Anton Mayer",
-    email: "antom@gmail.com",
-    phone: "+49 1111 111 11 1",
-}, {
-    name: "Anja Schulz",
-    email: "schulz@hotmail.com",
-    phone: "+49 1111 111 11 1",
-}, {
-    name: "Benedikt Ziegler",
-    email: "benedikt@gmail.com",
-    phone: "+49 1111 111 11 1",
-}, {
-    name: "David Eisenberg",
-    email: "davidberg@gmail.com",
-    phone: "+49 1111 111 11 1",
-}, {
-    name: "Eva Fischer",
-    email: "eva@gmail.com",
-    phone: "+49 1111 111 11 1",
-}, {
-    name: "Emmanuel Mauer",
-    email: "emmanuelma@gmail.com",
-    phone: "+49 1111 111 11 1",
-}, {
-    name: "Marcel Bauer",
-    email: "bauer@gmail.com",
-    phone: "+49 1111 111 11 1",
-}, {
-    name: "Tatiana Wolf",
-    email: "wolf@gmail.com",
-    phone: "+49 1111 111 11 1",
-}, ]
-let user01 = new Account("Anton Mayer", "antom@gmail.com", null, "+49 1111 111 11 1")
-Join.accounts.push(user01);
+        name: "Anton Mayer",
+        email: "antom@gmail.com",
+        phone: "+49 1111 111 11 1",
+    }, {
+        name: "Anja Schulz",
+        email: "schulz@hotmail.com",
+        phone: "+49 1111 111 11 1",
+    }, {
+        name: "Benedikt Ziegler",
+        email: "benedikt@gmail.com",
+        phone: "+49 1111 111 11 1",
+    }, {
+        name: "David Eisenberg",
+        email: "davidberg@gmail.com",
+        phone: "+49 1111 111 11 1",
+    }, {
+        name: "Eva Fischer",
+        email: "eva@gmail.com",
+        phone: "+49 1111 111 11 1",
+    }, {
+        name: "Emmanuel Mauer",
+        email: "emmanuelma@gmail.com",
+        phone: "+49 1111 111 11 1",
+    }, {
+        name: "Marcel Bauer",
+        email: "bauer@gmail.com",
+        phone: "+49 1111 111 11 1",
+    }, {
+        name: "Tatiana Wolf",
+        email: "wolf@gmail.com",
+        phone: "+49 1111 111 11 1",
+    }, ]
+    // let user01 = new Account("Anton Mayer", "antom@gmail.com", null, "+49 1111 111 11 1")
+    // Join.accounts.push(user01);
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // durch das split wird das Alphabet in ein Array von Buchstaben aufgeteilt.
 const colors = ['#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B'];
@@ -53,7 +53,7 @@ function renderContacts() {
             contact.color = colors[Math.floor(Math.random() * colors.length)];
         }
         if (userInitials !== currentInitials) {
-            // contactsList += `<div class="alphabet-section" id="alphabet-${userInitials}">${userInitials}</div>`;
+            contactsList += `<div class="alphabet-section" id="alphabet-${userInitials}">${userInitials}</div>`;
             currentInitials = userInitials;
         }
 
