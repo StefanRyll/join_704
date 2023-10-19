7245c7e49e00040ad0a921170e563a9872e09a18
StefanRyll <142165351+StefanRyll@users.noreply.github.com>
2023-10-19 14:18:19 +0200

    update contacts

--- index/Main/contacts/contacts.js
+++ index/Main/contacts/contacts.js
@@ -2,40 +2,64 @@ let user = [{
         name: "Anton Mayer",
         email: "antom@gmail.com",
         phone: "+49 1111 111 11 1",
-    }, {
+    },
+    {
         name: "Anja Schulz",
         email: "schulz@hotmail.com",
         phone: "+49 1111 111 11 1",
-    }, {
+    },
+    {
         name: "Benedikt Ziegler",
         email: "benedikt@gmail.com",
         phone: "+49 1111 111 11 1",
-    }, {
+    },
+    {
         name: "David Eisenberg",
         email: "davidberg@gmail.com",
         phone: "+49 1111 111 11 1",
-    }, {
+    },
+    {
         name: "Eva Fischer",
         email: "eva@gmail.com",
         phone: "+49 1111 111 11 1",
-    }, {
+    },
+    {
         name: "Emmanuel Mauer",
         email: "emmanuelma@gmail.com",
         phone: "+49 1111 111 11 1",
-    }, {
+    },
+    {
         name: "Marcel Bauer",
         email: "bauer@gmail.com",
         phone: "+49 1111 111 11 1",
-    }, {
+    },
+    {
         name: "Tatiana Wolf",
         email: "wolf@gmail.com",
         phone: "+49 1111 111 11 1",
-    }, ]
-    // let user01 = new Account("Anton Mayer", "antom@gmail.com", null, "+49 1111 111 11 1")
-    // Join.accounts.push(user01);
+    },
+];
+// let user01 = new Account("Anton Mayer", "antom@gmail.com", null, "+49 1111 111 11 1")
+// Join.accounts.push(user01);
 
-const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // durch das split wird das Alphabet in ein Array von Buchstaben aufgeteilt.
-const colors = ['#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B'];
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // durch das split wird das Alphabet in ein Array von Buchstaben aufgeteilt.
+const colors = [
+    "#FF7A00",
+    "#FF5EB3",
+    "#6E52FF",
+    "#9327FF",
+    "#00BEE8",
+    "#1FD7C1",
+    "#FF745E",
+    "#FFA35E",
+    "#FC71FF",
+    "#FFC701",
+    "#0038FF",
+    "#C3FF2B",
+    "#FFE62B",
+    "#FF4646",
+    "#FFBB2B",
+];
 
 async function initContacts() {
     await includeHTML();
@@ -43,8 +67,8 @@ async function initContacts() {
 }
 
 function renderContacts() {
-    let contactsList = '';
-    let currentInitials = '';
+    let contactsList = "";
+    let currentInitials = "";
 
     user.sort((a, b) => a.name.localeCompare(b.name)); //user wird nach dem a und b prinzip sortiert. localeCompare ist eine Methode, die verwendet wird, um Zeichenfolgen miteinander zu vergleichen, um die richtige Reihenfolge für die Sortierung festzustellen.
 
@@ -52,13 +76,15 @@ function renderContacts() {
         const contact = user[i];
         const userInitials = getInitials(contact.name);
         contact.color = getColor(contact.name); // .color ist eine CSS eigenschaft die später die farben ausdem array colors verwertet.
-        if (!contact.color) { // bedingung: hat der contact noch keine farbe (!).
+        if (!contact.color) {
+            // bedingung: hat der contact noch keine farbe (!).
             contact.color = colors[Math.floor(Math.random() * colors.length)]; //es wird eine zufällige farbe aus dem array colors ausgewählt.
         }
         if (userInitials[0] !== currentInitials) {
-            if (alphabet.includes(userInitials[0])) { // hier wird überprüft ob der anfangsbuchstabe aus dem userinitials bereits da ist.
-                if (contactsList !== '') {
-                    contactsList += '</div>';
+            if (alphabet.includes(userInitials[0])) {
+                // hier wird überprüft ob der anfangsbuchstabe aus dem userinitials bereits da ist.
+                if (contactsList !== "") {
+                    contactsList += "</div>";
                 }
                 contactsList += `<div class="alphabet-section" id="alphabet-${userInitials[0]}">${userInitials[0]}</div>`;
                 currentInitials = userInitials[0];
@@ -79,18 +105,15 @@ function renderContacts() {
         `;
     }
 
-    if (contactsList !== '') {
-        contactsList += '</div>';
+    if (contactsList !== "") {
+        contactsList += "</div>";
     }
 
-    document.getElementById('contactsList').innerHTML = contactsList;
+    document.getElementById("contactsList").innerHTML = contactsList;
 }
 
-
-
-
 function getInitials(name) {
-    const parts = name.split(' ');
+    const parts = name.split(" ");
     let initials = parts[0][0];
     if (parts.length > 1) {
         initials += parts[parts.length - 1][0];
@@ -99,7 +122,7 @@ function getInitials(name) {
 }
 
 function getColor(name) {
-    const sum = name.split('').reduce((sum, char) => sum + char.charCodeAt(0), 0); //methode um die ersten Buchstaben in zahlen zu umwandeln.
+    const sum = name.split("").reduce((sum, char) => sum + char.charCodeAt(0), 0); //methode um die ersten Buchstaben in zahlen zu umwandeln.
     const colorIndex = sum % colors.length; // hier werden die zahlen zusammen addiert und der array colors zusammenberechnet
     return colors[colorIndex];
 }
@@ -109,16 +132,17 @@ function showDetails(i) {
     const userInitials = getInitials(contact.name);
     const detailsContent = /* html */ `
     <div class="contactView">
-        <div class="detailsLogo" style="background-color: ${contact.color}; margin: 0 auto;">${userInitials}</div>
+        <div class="initials-logo logo-by-details" style="background-color: ${contact.color}; margin: 0 auto;">${userInitials}</div>
         <div class="name">
-            <h3>${contact.name}</h3>
-            <div class="contactsIcons">
+            <h2 class="name-headline">${contact.name}</h2>
+            <a class="contactsIcons">
                 <div class="editIcon">
                     <img class="editSymbol" src="../IMG/edit.png"> <span>Edit</span>
                 </div>
                 <div class="deleteIcon">
                     <img src="../IMG/delete.png"> <span>Delete </span>
                 </div>
+            </a>
         </div>
     </div>
     <div class="contactInformation">
@@ -129,19 +153,20 @@ function showDetails(i) {
         <p>Phone: ${contact.phone}</p>
     </div>
 `;
-    document.getElementById('detailsContainer').innerHTML = detailsContent;
+    document.getElementById("detailsContainer").innerHTML = detailsContent;
 }
 
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]'); //  Wir rufen unseren DIV im Index auf 
-    for (let i = 0; i < includeElements.length; i++) { // hier iterieren wir alles Obejekte in diesem DIV container sprich alles was in dem Fall im Header Bereich ist 
+    let includeElements = document.querySelectorAll("[w3-include-html]"); //  Wir rufen unseren DIV im Index auf
+    for (let i = 0; i < includeElements.length; i++) {
+        // hier iterieren wir alles Obejekte in diesem DIV container sprich alles was in dem Fall im Header Bereich ist
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // hier liest er den Wert im Index.Html "includes/header.html" aus. Und wir deieser Varibale file zugeordnet 
-        let resp = await fetch(file); // hier laden wir die datei mit fetch 
+        file = element.getAttribute("w3-include-html"); // hier liest er den Wert im Index.Html "includes/header.html" aus. Und wir deieser Varibale file zugeordnet
+        let resp = await fetch(file); // hier laden wir die datei mit fetch
         if (resp.ok) {
             element.innerHTML = await resp.text(); // hier haben wir jetzt alles in der Variable contetn als Text gespeichert^
         } else {
-            element.innerHTML = 'Page not found';
+            element.innerHTML = "Page not found";
         }
     }
 }
