26721391cfb00d988499a5228c69adb0919fc759
StefanRyll <142165351+StefanRyll@users.noreply.github.com>
2023-10-22 15:45:21 +0200

    update contacts overlay

--- index/Main/contacts/contacts.js
+++ index/Main/contacts/contacts.js
@@ -212,28 +212,14 @@ function openOverlay() {
 function generateHtmlAddContact() {
     return /*html*/ `
     <div class="overlay-container slide-in">
-        <div class="overlay-left-container">
-            <img src="../IMG/LogoWhite.png" alt="join-logo">
-            <h1 class="add-contact-headline">Add contact</h1>
-            <span>Tasks are better with a team</span>
-            <img src="../IMG/vector-5.png">
-        </div>
+        ${generateLeftSide()}
         <div class ="overlay-mid-container-addContact">
             <div class="add-Contact-logo">
                 <img src="../IMG/person-white.png">
             </div>
         </div>
         <div class="overlay-right-container-addContact">
-            <div class="style-closebutton">
-                <svg onclick="closeOverlay()" class="close-button-add-task cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <mask id="mask0_87491_5574" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                        <rect width="24" height="24" fill="#D9D9D9"/>
-                    </mask>
-                    <g mask="url(#mask0_87491_5574)">
-                        <path d="M12 13.4L7.1 18.3C6.91667 18.4834 6.68333 18.575 6.4 18.575C6.11667 18.575 5.88333 18.4834 5.7 18.3C5.51667 18.1167 5.425 17.8834 5.425 17.6C5.425 17.3167 5.51667 17.0834 5.7 16.9L10.6 12L5.7 7.10005C5.51667 6.91672 5.425 6.68338 5.425 6.40005C5.425 6.11672 5.51667 5.88338 5.7 5.70005C5.88333 5.51672 6.11667 5.42505 6.4 5.42505C6.68333 5.42505 6.91667 5.51672 7.1 5.70005L12 10.6L16.9 5.70005C17.0833 5.51672 17.3167 5.42505 17.6 5.42505C17.8833 5.42505 18.1167 5.51672 18.3 5.70005C18.4833 5.88338 18.575 6.11672 18.575 6.40005C18.575 6.68338 18.4833 6.91672 18.3 7.10005L13.4 12L18.3 16.9C18.4833 17.0834 18.575 17.3167 18.575 17.6C18.575 17.8834 18.4833 18.1167 18.3 18.3C18.1167 18.4834 17.8833 18.575 17.6 18.575C17.3167 18.575 17.0833 18.4834 16.9 18.3L12 13.4Z" fill="#2A3647"/>
-                    </g>
-                </svg>
-            </div>
+            ${closeButton ()}
         </div>
     </div>
  
@@ -243,12 +229,7 @@ function generateHtmlAddContact() {
 function generateHtmlEditContact() {
     return /*html*/ `
     <div class="overlay-container slide-in">
-        <div class="overlay-left-container">
-            <img src="../IMG/LogoWhite.png" alt="join-logo">
-            <h1 class="add-contact-headline">Add contact</h1>
-            <span>Tasks are better with a team</span>
-            <img src="../IMG/vector-5.png">
-        </div>
+        ${generateLeftSide()}
         <div class ="">
 
         </div>
@@ -260,12 +241,7 @@ function generateHtmlEditContact() {
 function generateHtmlDeleteContact() {
     return /*html*/ `
     <div class="overlay-container slide-in">
-        <div class="overlay-left-container">
-            <img src="../IMG/LogoWhite.png" alt="join-logo">
-            <h1 class="add-contact-headline">Add contact</h1>
-            <span>Tasks are better with a team</span>
-            <img src="../IMG/vector-5.png">
-        </div>
+        ${generateLeftSide()}
         <div class ="">
 
         </div>
@@ -274,6 +250,39 @@ function generateHtmlDeleteContact() {
     `
 }
 
+/**
+ * templates for the overlay
+ * @returns to the generate functions
+ */
+function generateLeftSide() {
+    return /*html*/ `
+        <div class="overlay-left-container">
+            <img src="../IMG/LogoWhite.png" alt="join-logo">
+            <h1 class="add-contact-headline">Add contact</h1>
+            <span>Tasks are better with a team</span>
+            <img src="../IMG/vector-5.png">
+        </div>
+    `
+}
+
+function closeButton() {
+    return /*html*/ `
+        <div class="style-closebutton">
+            <svg onclick="closeOverlay()" class="close-button-add-task cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <mask id="mask0_87491_5574" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                    <rect width="24" height="24" fill="#D9D9D9"/>
+                </mask>
+                <g mask="url(#mask0_87491_5574)">
+                    <path d="M12 13.4L7.1 18.3C6.91667 18.4834 6.68333 18.575 6.4 18.575C6.11667 18.575 5.88333 18.4834 5.7 18.3C5.51667 18.1167 5.425 17.8834 5.425 17.6C5.425 17.3167 5.51667 17.0834 5.7 16.9L10.6 12L5.7 7.10005C5.51667 6.91672 5.425 6.68338 5.425 6.40005C5.425 6.11672 5.51667 5.88338 5.7 5.70005C5.88333 5.51672 6.11667 5.42505 6.4 5.42505C6.68333 5.42505 6.91667 5.51672 7.1 5.70005L12 10.6L16.9 5.70005C17.0833 5.51672 17.3167 5.42505 17.6 5.42505C17.8833 5.42505 18.1167 5.51672 18.3 5.70005C18.4833 5.88338 18.575 6.11672 18.575 6.40005C18.575 6.68338 18.4833 6.91672 18.3 7.10005L13.4 12L18.3 16.9C18.4833 17.0834 18.575 17.3167 18.575 17.6C18.575 17.8834 18.4833 18.1167 18.3 18.3C18.1167 18.4834 17.8833 18.575 17.6 18.575C17.3167 18.575 17.0833 18.4834 16.9 18.3L12 13.4Z" fill="#2A3647"/>
+                </g>
+            </svg>
+        </div>
+    `
+}
+
+/**
+ *  function for close the overlays
+ */
 function closeOverlay() {
     let addContactForm = document.getElementById('overlay');
     addContactForm.classList.add('slide-out')
